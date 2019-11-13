0a4257d060e007e32e3716fba0906326686e13f8
sontx <xuanson33bk@gmail.com>
2019-11-13 23:54:17 +0700

    + inputbox supports for simple value such as string, number...

---
+++ Blackcat.WinForm/InputBox.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Blackcat.WinForm
+{
+    partial class InputBox
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labDescription = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.pnlWorkingSpace.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlWorkingSpace
+            // 
+            this.pnlWorkingSpace.Controls.Add(this.labDescription);
+            this.pnlWorkingSpace.Controls.Add(this.txtInput);
+            this.pnlWorkingSpace.Padding = new System.Windows.Forms.Padding(10, 8, 10, 18);
+            this.pnlWorkingSpace.Size = new System.Drawing.Size(389, 108);
+            // 
+            // pnlControlSpace
+            // 
+            this.pnlControlSpace.Location = new System.Drawing.Point(0, 108);
+            this.pnlControlSpace.Size = new System.Drawing.Size(389, 42);
+            // 
+            // labDescription
+            // 
+            this.labDescription.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labDescription.Location = new System.Drawing.Point(10, 8);
+            this.labDescription.Name = "labDescription";
+            this.labDescription.Padding = new System.Windows.Forms.Padding(0, 0, 0, 3);
+            this.labDescription.Size = new System.Drawing.Size(369, 60);
+            this.labDescription.TabIndex = 0;
+            this.labDescription.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            // 
+            // txtInput
+            // 
+            this.txtInput.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.txtInput.Location = new System.Drawing.Point(10, 68);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.txtInput.Size = new System.Drawing.Size(369, 22);
+            this.txtInput.TabIndex = 1;
+            // 
+            // InputBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(389, 150);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Sizable;
+            this.Name = "InputBox";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "InputBox";
+            this.pnlWorkingSpace.ResumeLayout(false);
+            this.pnlWorkingSpace.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Label labDescription;
+    }
+}
---
+++ Blackcat.WinForm/InputBox.cs
@@ -0,0 +1,60 @@
+﻿using Newtonsoft.Json;
+
+namespace Blackcat.WinForm
+{
+    public partial class InputBox : ChooserForm
+    {
+        public static T Show<T>(string title, string description, T defaultValue)
+        {
+            using (var form = new InputBox { Text = title, Description = description })
+            {
+                if (form.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    return form.GetValue<T>(defaultValue);
+                }
+            }
+            return defaultValue;
+        }
+
+        private string description;
+
+        public string Description
+        {
+            get => description;
+            set
+            {
+                description = value;
+                labDescription.Text = value;
+            }
+        }
+
+        public object Value
+        {
+            get => txtInput.Text.Trim();
+            set
+            {
+                txtInput.Text = value != null ? value.ToString() : "";
+            }
+        }
+
+        public InputBox()
+        {
+            InitializeComponent();
+        }
+
+        public T GetValue<T>(T defaultValue)
+        {
+            try
+            {
+                var st = txtInput.Text.Trim() as object;
+                if (typeof(T) == typeof(string))
+                {
+                    return (T)st;
+                }
+                return JsonConvert.DeserializeObject<T>(st.ToString());
+            }
+            catch { }
+            return defaultValue;
+        }
+    }
+}
