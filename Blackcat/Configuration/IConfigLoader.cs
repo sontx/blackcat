18b7cf5ccdb7e3e3b95665f8bee3c54348ab065a
sontx <xuanson33bk@gmail.com>
2019-12-25 10:03:25 +0700

    + document for IConfigLoader

--- Blackcat/Configuration/IConfigLoader.cs
+++ Blackcat/Configuration/IConfigLoader.cs
@@ -4,9 +4,25 @@ namespace Blackcat.Configuration
 {
     public interface IConfigLoader : IDisposable
     {
+        /// <summary>
+        /// When changes will be saved
+        /// </summary>
         SaveMode SaveMode { get; set; }
+
+        /// <summary>
+        /// How changes will be encoded or decoded
+        /// </summary>
         IDataAdapter Adapter { get; set; }
 
+        /// <summary>
+        /// How encoded changes will be saved
+        /// </summary>
+        IDataStorage Storage { get; set; }
+
+        /// <summary>
+        /// Gets configuration instance by its type
+        /// </summary>
+        /// <typeparam name="T">Configuration type</typeparam>
         T Get<T>() where T : class;
     }
 }
